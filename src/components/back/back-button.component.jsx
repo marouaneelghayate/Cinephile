d1713458802891a0df4ee535fa5bcafabde1b562
marouane <elghayate28@gmail.com>
2023-09-24 04:47:14 +0100

    second commit

--- src/components/back/back-button.component.jsx
+++ src/components/back/back-button.component.jsx
@@ -1,10 +1,13 @@
-import { useNavigate } from "react-router-dom"
-import { BackButton } from "./back-button.style"
+import { useNavigate } from 'react-router-dom';
+import { BackButton } from './back-button.style';
 
-const Back = ()=>{
-    const navigate = useNavigate();
-    return <BackButton onClick={()=>navigate(-1)}>Back</BackButton>;
-    
-}
+const Back = () => {
+  const navigate = useNavigate();
+  return (
+    <BackButton onClick={() => navigate(-1)}>
+        <span className="arrow"></span> Go back
+    </BackButton>
+  );
+};
 
 export default Back;
--- src/components/back/back-button.style.js
+++ src/components/back/back-button.style.js
@@ -1,4 +1,57 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const BackButton=styled.button`
+export const BackButton = styled.button`
+  position: absolute;
+  top: 18px;
+  left: 40px;
+  display: flex;
+  align-items: center;
+  padding: 12px 15px 12px 20px;
+  border-radius: 20px;
+  color: #fff;
+  background: none;
+  font-size: 14px;
+  font-weight: bold;
+  border: none;
+  transition: 0.3s;
+  cursor: pointer;
+
+  .arrow {
+    width: 6px;
+    height: 6px;
+    margin-right: 3px;
+    border-color: #fff;
+    border-left: 2px solid;
+    border-top: 2px solid;
+    position: relative;
+    rotate: -45deg;
+    transition: 0.3s;
+
+
+    &:before {
+      content: '';
+      position: absolute;
+      right: 0;
+      bottom: -1px;
+      rotate: 45deg;
+      width: 3px;
+      background-color: #fff;
+      transform-origin: bottom right;
+      height: 2px;
+      opacity: 0;
+      transition: .3s;
+    }
+  }
+  &:hover {
+    translate: 3px;
+    background: hsla(0, 0%, 50%, .5);
+    .arrow {
+      translate: -7px;
+
+      &:before {
+        opacity: 1;
+        width: 10px;
+      }
+    }
+  }
 `;
